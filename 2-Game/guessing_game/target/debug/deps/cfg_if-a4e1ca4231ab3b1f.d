5c888b02fac653e281edebffdb2c4a1b172620db
belligerentcrow <deadblink333@gmail.com>
2024-03-02 12:01:21 +0100

    removed some cargo

--- 2-Game/guessing_game/target/debug/deps/cfg_if-a4e1ca4231ab3b1f.d
+++
@@ -1,7 +0,0 @@
-/home/fed/Documents/Github/rust-y/2-Game/guessing_game/target/debug/deps/libcfg_if-a4e1ca4231ab3b1f.rmeta: /home/fed/.cargo/registry/src/index.crates.io-6f17d22bba15001f/cfg-if-1.0.0/src/lib.rs
-
-/home/fed/Documents/Github/rust-y/2-Game/guessing_game/target/debug/deps/libcfg_if-a4e1ca4231ab3b1f.rlib: /home/fed/.cargo/registry/src/index.crates.io-6f17d22bba15001f/cfg-if-1.0.0/src/lib.rs
-
-/home/fed/Documents/Github/rust-y/2-Game/guessing_game/target/debug/deps/cfg_if-a4e1ca4231ab3b1f.d: /home/fed/.cargo/registry/src/index.crates.io-6f17d22bba15001f/cfg-if-1.0.0/src/lib.rs
-
-/home/fed/.cargo/registry/src/index.crates.io-6f17d22bba15001f/cfg-if-1.0.0/src/lib.rs:
--- 2-Game/guessing_game/target/debug/deps/guessing_game-3fb26747fda5a286.d
+++
@@ -1,5 +0,0 @@
-/home/fed/Documents/Github/rust-y/2-Game/guessing_game/target/debug/deps/guessing_game-3fb26747fda5a286: src/main.rs
-
-/home/fed/Documents/Github/rust-y/2-Game/guessing_game/target/debug/deps/guessing_game-3fb26747fda5a286.d: src/main.rs
-
-src/main.rs:
